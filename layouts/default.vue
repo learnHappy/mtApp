bdd20cddbbe7a5afe17057977a79fd80bb037930
pengzj <3448416037@qq.com>
2020-02-26 16:29:52 +0800

    公共头部

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+    <el-container>
+      <el-header><my-header/></el-header>
+      <el-main><nuxt /></el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
 </template>
+
+<script>
+  import MyHeader from '../components/public/header/index'
+
+  export default {
+    components: {
+      MyHeader
+    }
+  }
+
+</script>
 
 <style>
 html {
